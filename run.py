07d6338558a860d437ec844f443ecfe80d698846
Josback <backelantsjoseph@gmail.com>
2025-03-31 15:41:29 +0100

    comment call of server flask

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    logging.info("Flask app started")
-    redis_client = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
-    app.run(host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     logging.info("Flask app started")
+#     redis_client = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
+#     app.run(host="0.0.0.0", port=8000)
