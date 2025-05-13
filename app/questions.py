411a2ddeaff3598924704d59e5769ab475cdb562
Josback <backelantsjoseph@gmail.com>
2025-05-13 06:49:04 +0100

    uncomment questions

--- app/questions.py
+++ app/questions.py
@@ -1,87 +1,78 @@
 QUESTIONS = {
     "Génération de la valeur": [ 
        " Bienvenu chez I&F Entrepreneuriat ! 😊 Je suis votre Coach et ensemble, on va collecter quelques informations pour vous aider à élaborer votre candidature COPA. 💼 Vous êtes prêt ? 🚀",
-        "Pouvez-vous me parler de vous en tant qu'entrepreneur ? Quel est votre parcours, votre formation, et qu'est-ce qui vous a motivé à vous lancer dans ce projet ?",]
-    #     "Quelles sont les difficultés que vous avez rencontrées jusqu'à présent dans votre travail, et comment comptez-vous les surmonter ?",
-    #     "Quels sont ses points forts et comment compte-t-il les utiliser dans l’entreprise ?",
-    #             "Qui sont les personnes qui vous soutiennent dans ce projet, et en quoi leur soutien est-il important pour vous ?",
-    #     "Pouvez-vous me décrire votre entreprise en quelques mots ? Quel est son objectif principal et à quel stade de développement se trouve-t-elle actuellement ?",
-    #     "Comment est organisée votre équipe managériale ? Qui sont les associés et quels sont leurs rôles dans l'entreprise ?",
-    # ],
-    # "Présentation de l'entreprise":[
-    #     "Qui sont les personnes qui vous soutiennent dans ce projet, et en quoi leur soutien est-il important pour vous ?",
-    #     "Pouvez-vous me décrire votre entreprise en quelques mots ? Quel est son objectif principal et à quel stade de développement se trouve-t-elle actuellement ?",
-    #     "Comment est organisée votre équipe managériale ? Qui sont les associés et quels sont leurs rôles dans l'entreprise ?",
-    #     "Quelles sont les principales forces, faiblesses, opportunités et menaces que vous avez identifiées ?"
-    #     ],
-    #     "Présentation de l'offre de produit(s) ou service(s)":[
-    #         "Pouvez-vous me décrire en détail le produit ou le service que vous proposez ? Quels sont ses caractéristiques et ses avantages par rapport à ce qui existe déjà sur le marché ?",
-    #         "À quel besoin spécifique répond votre produit ou service ? Comment pensez-vous qu'il va améliorer la vie de vos clients ?",
-    #         "Qui sont vos principaux concurrents, et en quoi votre offre est-elle différente ou meilleure que la leur ?"
-    #     ],
-    #     "Environnement du projet":[
-    #         "Comment prenez-vous en compte les aspects sociaux et écologiques dans votre projet ? Par exemple, comment gérez-vous les déchets ou les impacts environnementaux de votre activité ?",
-    #         "Quelles sont les habitudes de consommation de votre marché cible, et comment votre produit s'adapte-t-il à ces habitudes ?"
-    #     ],
-    #     "Rémuneration de la valeur ": [
-    #         "Pouvez-vous me donner une idée des investissements nécessaires pour démarrer votre projet ? Quels sont les principaux coûts que vous anticipez ?",
-    #         "Comment avez-vous calculé vos prévisions de ventes ? Est-ce que vous avez établi des scénarios optimistes et pessimistes ?",
-    #         "Avez-vous déjà obtenu des financements ou des subventions pour votre projet ? Si oui, de quelle nature sont-ils ?"
+        "Pouvez-vous me parler de vous en tant qu'entrepreneur ? Quel est votre parcours, votre formation, et qu'est-ce qui vous a motivé à vous lancer dans ce projet ?",
+        "Quelles sont les difficultés que vous avez rencontrées jusqu'à présent dans votre travail, et comment comptez-vous les surmonter ?",
+        "Quels sont ses points forts et comment compte-t-il les utiliser dans l’entreprise ?",
+                "Qui sont les personnes qui vous soutiennent dans ce projet, et en quoi leur soutien est-il important pour vous ?",
+        "Pouvez-vous me décrire votre entreprise en quelques mots ? Quel est son objectif principal et à quel stade de développement se trouve-t-elle actuellement ?",
+        "Comment est organisée votre équipe managériale ? Qui sont les associés et quels sont leurs rôles dans l'entreprise ?",
+    ],
+    "Présentation de l'entreprise":[
+        "Qui sont les personnes qui vous soutiennent dans ce projet, et en quoi leur soutien est-il important pour vous ?",
+        "Pouvez-vous me décrire votre entreprise en quelques mots ? Quel est son objectif principal et à quel stade de développement se trouve-t-elle actuellement ?",
+        "Comment est organisée votre équipe managériale ? Qui sont les associés et quels sont leurs rôles dans l'entreprise ?",
+        "Quelles sont les principales forces, faiblesses, opportunités et menaces que vous avez identifiées ?"
+        ],
+        "Présentation de l'offre de produit(s) ou service(s)":[
+            "Pouvez-vous me décrire en détail le produit ou le service que vous proposez ? Quels sont ses caractéristiques et ses avantages par rapport à ce qui existe déjà sur le marché ?",
+            "À quel besoin spécifique répond votre produit ou service ? Comment pensez-vous qu'il va améliorer la vie de vos clients ?",
+            "Qui sont vos principaux concurrents, et en quoi votre offre est-elle différente ou meilleure que la leur ?"
+        ],
+        "Environnement du projet":[
+            "Comment prenez-vous en compte les aspects sociaux et écologiques dans votre projet ? Par exemple, comment gérez-vous les déchets ou les impacts environnementaux de votre activité ?",
+            "Quelles sont les habitudes de consommation de votre marché cible, et comment votre produit s'adapte-t-il à ces habitudes ?"
+        ],
+        "Rémuneration de la valeur ": [
+            "Pouvez-vous me donner une idée des investissements nécessaires pour démarrer votre projet ? Quels sont les principaux coûts que vous anticipez ?",
+            "Comment avez-vous calculé vos prévisions de ventes ? Est-ce que vous avez établi des scénarios optimistes et pessimistes ?",
+            "Avez-vous déjà obtenu des financements ou des subventions pour votre projet ? Si oui, de quelle nature sont-ils ?"
 
-    #     ],
+        ],
 
-    # "Canaux de revenu": 
-    #      [
-    #         "Comment comptez-vous distribuer votre produit ? Allez-vous passer par des détaillants, des grossistes, ou vendre directement aux consommateurs ?",
-    #         "Quelle est votre stratégie de prix ? Comment avez-vous déterminé le prix de vente de votre produit ?",
-    #         "Avez-vous prévu des campagnes de communication pour promouvoir votre produit ? Si oui, quels canaux allez-vous utiliser ?"
-    #         "Le prix au grossiste revient à combien ?"
-    #     ],
-    #     "Partage de la valeur" : [
-    #         "Quels sont selon vous les éléments clés qui pourraient favoriser la réussite de votre projet ? Comment comptez-vous les exploiter ?",
-    #         "Quels sont les risques majeurs que vous identifiez pour votre projet, et comment comptez-vous les atténuer ?"
-    #     ],
-    #     "Parties prenantes":[
-    #         "Qui sont les principales parties prenantes de votre projet, et quel est leur rôle dans la réussite de celui-ci ?",
-    #         "Quelles sont les attentes de ces parties prenantes, et comment comptez-vous les satisfaire ?"
-    #     ],
-    #     "Banques et partenaires financiers":[
-    #         "Avez-vous déjà établi des contacts avec des banques ou des institutions financières pour obtenir des prêts ou des financements ? Si oui, quelles sont les conditions proposées ?",
-    #         "Comment comptez-vous gérer les remboursements d'emprunts ou les dividendes pour les investisseurs ?"
-    #     ],
-    #     "Gestion de fonds":[
-    #         "Comment comptez-vous gérer les salaires de vos employés et les coûts fixes de votre entreprise ? Avez-vous déjà prévu un budget pour cela ?",
-    #         "Comment allez-vous gérer les taxes et les marges bénéficiaires pour assurer la pérennité de votre entreprise ?"
-    #     ],
-    #     "Étude de marché" : [
-    #         "Avez-vous réalisé une étude de marché avant de lancer votre projet ? Si oui, quelles sont les principales conclusions que vous en avez tirées ?",
-    #         "Comment avez-vous identifié votre clientèle cible, et quelles sont leurs principales attentes ?"
-    #     ],
-    #     "Innovation et différenciation":[
-    #         "En quoi votre produit ou service est-il innovant par rapport à ce qui existe déjà sur le marché ? Quels sont les avantages concurrentiels que vous mettez en avant ?",
-    #         "Comment comptez-vous maintenir votre avantage concurrentiel à long terme ?"
-    #     ],
-    #     "Plannification et vision future":[
-    #         "Quelle est votre vision à long terme pour votre entreprise ? Où voyez-vous votre entreprise dans 5 ou 10 ans ?",
-    #         "Quels sont les prochains grands projets ou étapes que vous envisagez pour développer votre entreprise ?"
-    #     ],
-    #     "Politique de produit": [
-    #         "De quoi est composé votre produit (caractéristiques) ?",
-    #         "Comment est-il conditionné ou présenté et pour qui ?",
-    #         "Votre produit a-t-il un nom et que signifie-t-il ?",
-    #         "Comment l’utilise-t-on, quand et où ?",
-    #         "Quelles sont les précautions à prendre quand on l’utilise ?",
-    #         "Quelles précautions pour la conservation ?"
-    #     ],
-    #     "Politique des prix": [
-    #         "Quel est le prix unitaire de votre produit ?",
-    #         "Quelle est la marge unitaire bénéficiaire ?",
-    #         "Est-ce un prix égal au prix du marché ?",
-    #         "Si le prix est différent, pourquoi ?",
-    #         "Est-ce un prix fixé en fonction du coût de revient unitaire ?",
-    #         "Quel est le prix de vente unitaire hors taxe ?",
-    #         "Quel est le prix unitaire toutes taxes confondues ?"
-    #     ],
+    "Canaux de revenu": 
+         [
+            "Comment comptez-vous distribuer votre produit ? Allez-vous passer par des détaillants, des grossistes, ou vendre directement aux consommateurs ?",
+            "Quelle est votre stratégie de prix ? Comment avez-vous déterminé le prix de vente de votre produit ?",
+            "Avez-vous prévu des campagnes de communication pour promouvoir votre produit ? Si oui, quels canaux allez-vous utiliser ?"
+            "Le prix au grossiste revient à combien ?"
+        ],
+        "Partage de la valeur" : [
+            "Quels sont selon vous les éléments clés qui pourraient favoriser la réussite de votre projet ? Comment comptez-vous les exploiter ?",
+            "Quels sont les risques majeurs que vous identifiez pour votre projet, et comment comptez-vous les atténuer ?"
+        ],
+        "Parties prenantes":[
+            "Qui sont les principales parties prenantes de votre projet, et quel est leur rôle dans la réussite de celui-ci ?",
+            "Quelles sont les attentes de ces parties prenantes, et comment comptez-vous les satisfaire ?"
+        ],
+        "Banques et partenaires financiers":[
+            "Avez-vous déjà établi des contacts avec des banques ou des institutions financières pour obtenir des prêts ou des financements ? Si oui, quelles sont les conditions proposées ?",
+            "Comment comptez-vous gérer les remboursements d'emprunts ou les dividendes pour les investisseurs ?"
+        ],
+        "Gestion de fonds":[
+            "Comment comptez-vous gérer les salaires de vos employés et les coûts fixes de votre entreprise ? Avez-vous déjà prévu un budget pour cela ?",
+            "Comment allez-vous gérer les taxes et les marges bénéficiaires pour assurer la pérennité de votre entreprise ?"
+        ],
+        "Étude de marché" : [
+            "Avez-vous réalisé une étude de marché avant de lancer votre projet ? Si oui, quelles sont les principales conclusions que vous en avez tirées ?",
+            "Comment avez-vous identifié votre clientèle cible, et quelles sont leurs principales attentes ?"
+        ],
+        "Innovation et différenciation":[
+            "En quoi votre produit ou service est-il innovant par rapport à ce qui existe déjà sur le marché ? Quels sont les avantages concurrentiels que vous mettez en avant ?",
+            "Comment comptez-vous maintenir votre avantage concurrentiel à long terme ?"
+        ],
+        "Plannification et vision future":[
+            "Quelle est votre vision à long terme pour votre entreprise ? Où voyez-vous votre entreprise dans 5 ou 10 ans ?",
+            "Quels sont les prochains grands projets ou étapes que vous envisagez pour développer votre entreprise ?"
+        ],
+        "Politique de produit": [
+            "De quoi est composé votre produit (caractéristiques) ?",
+            "Comment est-il conditionné ou présenté et pour qui ?",
+            "Votre produit a-t-il un nom et que signifie-t-il ?",
+            "Comment l’utilise-t-on, quand et où ?",
+            "Quelles sont les précautions à prendre quand on l’utilise ?",
+            "Quelles précautions pour la conservation ?"
+        ],
     
     }
 
